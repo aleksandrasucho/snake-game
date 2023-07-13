21f8b31df2bb94b9b35979df7f58ede94a989087
aleksandrasucho <aleksandrasucho@gmail.com>
2023-07-13 22:02:22 +0100

    Add updates to game variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const gameGrid = document.querySelector(".game-grid");
     const scoreElement = document.querySelector(".score");
     const highScoreElement = document.querySelector(".high-score");
-  
-    // Click event listeners to the difficulty buttons
+    const gameControls = document.getElementById('controls');
     const difficultyBtnEasy = document.getElementById('easy-button');
     const difficultyBtnHard = document.getElementById('hard-button');
-  
+
+    const gridSize = 20; 
+    const gridWidth = gameScreen.offsetWidth / gridSize;
+    const gridHeight = gameScreen.offsetHeight / gridSize;
+
+    let snake = {
+    head: { x: 0, y: 0 },
+    tail: [],
+    direction: "right",
+  };
+
+  const food = {
+    x: Math.floor(Math.random() * gridWidth),
+    y: Math.floor(Math.random() * gridHeight),
+  };
+
+  let score = 0;
+  let highScore = 0;
+
+
     // Click event listeners to the difficulty buttons
     difficultyBtnEasy.addEventListener("click", function () {
       startGame("easy");
@@ -21,32 +39,24 @@ document.addEventListener("DOMContentLoaded", function () {
     difficultyBtnHard.addEventListener("click", function () {
       startGame("hard");
     });
-  
-    // Rest of the code...
-  
     // Function to start the game based on the chosen difficulty
     function startGame(difficulty) {
       // Hide the menu screen and show the game screen
       menuScreen.style.display = "none";
       gameScreen.style.display = "block";
   
-      // Add your game initialization logic here based on the chosen difficulty
       if (difficulty === "easy") {
-        // Easy game logic
-        console.log("Starting easy game...");
+        // Easy game 
+        console.log("Starting easy game");
       } else if (difficulty === "hard") {
-        // Hard game logic
-        console.log("Starting hard game...");
+        // Hard game 
+        console.log("Starting hard game");
       }
     }
   });
+ 
+ 
   
-  const gridSize = 20;
-  let snake = {
-    head: { x: 0, y: 0 },
-    tail: [],
-    direction: "right",
-  };
   
   // Calculate gridWidth and gridHeight after DOMContentLoaded event
   let gridWidth, gridHeight;
