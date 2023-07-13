8a5f9b4ca0842613129c22bccace5eec545b6923
aleksandrasucho <aleksandrasucho@gmail.com>
2023-07-13 11:57:45 +0100

    Add event listeners to difficulty buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function () {
     // game variables
-    const difficultyBtnEasy = document.getElementById('easy-button');
-    const difficultyBtnHard = document.getElementById('hard-button');
+    const menuScreen = document.getElementById('menu-screen');
     const gameScreen = document.getElementById('game-screen');
     const gameOverScreen = document.getElementById('game-over-screen');
     const modalText = document.getElementById('modal-result');
@@ -10,22 +9,50 @@ document.addEventListener("DOMContentLoaded", function () {
     const scoreElement = document.querySelector(".score");
     const highScoreElement = document.querySelector(".high-score");
   
+    // Click event listeners to the difficulty buttons
+    const difficultyBtnEasy = document.getElementById('easy-button');
+    const difficultyBtnHard = document.getElementById('hard-button');
+  
+    // Click event listeners to the difficulty buttons
+    difficultyBtnEasy.addEventListener("click", function () {
+      startGame("easy");
+    });
+  
+    difficultyBtnHard.addEventListener("click", function () {
+      startGame("hard");
+    });
+  
+    // Rest of the code...
+  
+    // Function to start the game based on the chosen difficulty
+    function startGame(difficulty) {
+      // Hide the menu screen and show the game screen
+      menuScreen.style.display = "none";
+      gameScreen.style.display = "block";
+  
+      // Add your game initialization logic here based on the chosen difficulty
+      if (difficulty === "easy") {
+        // Easy game logic
+        console.log("Starting easy game...");
+      } else if (difficulty === "hard") {
+        // Hard game logic
+        console.log("Starting hard game...");
+      }
+    }
   });
   
   const gridSize = 20;
-  const gridWidth = gameScreen.offsetWidth / gridSize;
-  const gridHeight = gameScreen.offsetHeight / gridSize;
-  
   let snake = {
     head: { x: 0, y: 0 },
     tail: [],
     direction: "right",
   };
- // Click event listeners to the difficulty buttons
- easyButton.addEventListener("click", function () {
-    startGame("easy");
+  
+  // Calculate gridWidth and gridHeight after DOMContentLoaded event
+  let gridWidth, gridHeight;
+  window.addEventListener("DOMContentLoaded", function () {
+    const gameScreen = document.getElementById('game-screen');
+    gridWidth = gameScreen.offsetWidth / gridSize;
+    gridHeight = gameScreen.offsetHeight / gridSize;
   });
-
-  hardButton.addEventListener("click", function () {
-    startGame("hard");
-  });
+  
