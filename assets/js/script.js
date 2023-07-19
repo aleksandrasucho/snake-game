8ae9a6a405dcf3c12185692c35c1ad8746f59371
aleksandrasucho <aleksandrasucho@gmail.com>
2023-07-19 17:41:49 +0100

    Add snake function to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const difficultyBtnEasy = document.getElementById('easy-button');
   const difficultyBtnHard = document.getElementById('hard-button');
 
-  const gridSize = 20; 
-  const gridWidth = gameScreen.offsetWidth / gridSize;
-  const gridHeight = gameScreen.offsetHeight / gridSize;
-
   // Game variables
   let squares = [];
   let currentSnake = [2, 1, 0];
@@ -69,25 +65,10 @@ document.addEventListener("DOMContentLoaded", function () {
  interval = setInterval(move, intervalTime);
 
 
-
-
-
-
-
-
-  // Function to create the game grid
-  function createGrid() {
-    for (let i = 0; i < width * width; i++) {
-      const square = document.createElement('div');
-      square.classList.add('box');
-      gameGrid.appendChild(square);
-      squares.push(square);
-    }
-  }
-  createGrid();
-
-// Generate a new apple on the grid
- function generateApple() {
+/**
+ * Function that generate a new apple on the grid
+ */
+function generateApple() {
   do {
     appleIndex = Math.floor(Math.random() * squares.length);
   } while (squares[appleIndex].classList.contains('snake'));
@@ -105,12 +86,74 @@ document.addEventListener("DOMContentLoaded", function () {
 generateApple();
 
 
+  // Function to create the game grid
+  function createGrid() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.classList.add('box');
+      gameGrid.appendChild(square);
+      squares.push(square);
+    }
+  }
 
+// Set the initial position of the snake
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
+/**
+ * Function that generate a new apple on the grid
+ */
+ function generateApple() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains('snake'));
+  let isAppleOver = false;
+  for (let i = 0; i < currentSnake.length; i++) {
+    if (currentSnake[i] == appleIndex) {
+      isAppleOver = true;
+    }
+  }
+  if (isAppleOver) {
+    generateApple();
+  }
+  squares[appleIndex].classList.add('apple');
+}
+generateApple();
 
+/**
+ * Function to move the snake
+ */
+function move() {
+  // Check for collisions with the boundaries or itself
+  if (
+    (currentSnake[0] % width === 0 && direction === -1) || // Hit left wall
+    (currentSnake[0] % width === width - 1 && direction === 1) || // Hit right wall
+    (currentSnake[0] - width < 0 && direction === -width) || // Hit top wall
+    (currentSnake[0] + width >= width * width && direction === width) || // Hit bottom wall
+    squares[currentSnake[0] + direction].classList.contains('snake')
+  ) {
+    clearInterval(interval);
+    modalText.textContent = `Your score: ${score}`;
+    gameOverScreen.style.display = 'block';
+    return;
+  }
 
+  // Remove the last element of the snake array
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove('snake');
 
+  // Add a new head to the snake array based on the direction
+  currentSnake.unshift(currentSnake[0] + direction);
+  squares[currentSnake[0]].classList.add('snake');
 
+  // Check if the snake has eaten the apple
+  if (currentSnake[0] === appleIndex) {
+    squares[appleIndex].classList.remove('apple');
+    currentSnake.push(tail); 
+    score += 10;
+    scoreElement.textContent = `Score: ${score}`;
+    generateApple();
+  }
+}
 
 
 
